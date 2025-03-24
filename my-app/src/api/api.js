6b21af766162a267fca4d407d953cfe27c7ef098
Jon H <jon.helge@skjaerstein.com>
2025-03-24 20:21:33 +0000

    updated api string

--- my-app/src/api/api.js
+++ my-app/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Use environment variable directly for Vite
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "https://api.skjaerstein.com/api/users",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://testarenaapi-production.up.railway.app/api/users",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
