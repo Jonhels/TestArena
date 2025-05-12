7c6d9b61d19204a91d7fc5cc5f50a4063e8d23d8
jonhels <jonhels8888@gmail.com>
2025-05-12 23:02:03 +0200

    adjusted header to match responsiveness to rest of page

--- my-app/src/components/InquiryComponents/InquiryHeader.jsx
+++ my-app/src/components/InquiryComponents/InquiryHeader.jsx
@@ -5,18 +5,19 @@ const InquiryHeader = ({ inquiries = [] }) => {
   const newCount = inquiries.filter((inq) => inq.status === "ulest").length;
 
   return (
-    <div className="inquiry-summary">
-      <div>
+    <div className="inquiry-header-wrapper">
+      <div className="inquiry-summary">
         <h2 className="inquiry-title">Henvendelser</h2>
         <p className="inquiry-subtext">
           Her finner du en oversikt over henvendelser. Velg en henvendelse for å se detaljer.
         </p>
       </div>
       <div className="inquiry-badge">
-        <span>Nye:</span>
+        <span className="label-badge">Nye:</span>
         <span className="inquiry-badge-count">{newCount}</span>
       </div>
     </div>
+
   );
 };
 
